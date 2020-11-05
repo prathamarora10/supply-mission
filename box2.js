822a59ca4f4679d2269652442808a55c8981d2f3
prathamarora10 <72607002+prathamarora10@users.noreply.github.com>
2020-11-05 12:32:34 +0530

    Add files via upload

---
+++ box2.js
@@ -0,0 +1,19 @@
+class Wall2
+{
+     constructor(x,y){
+
+      var option = {
+         isStatic: true
+      }
+
+        this.body = Bodies.rectangle(x,y,10,100,option);
+        World.add(world,this.body)
+     }
+        display(){
+            var pos = this.body.position
+            rectMode(CENTER);
+            fill('red')
+            rect(pos.x,pos.y,10,100);
+        }
+     
+}
